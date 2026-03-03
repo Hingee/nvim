25dabae6baa57b9e846b703c60cde4ad211c814f
Hingee <nathan.hingee@gmail.com>
2026-03-03 15:59:37 +1100

    Fixed

--- init.lua
+++ init.lua
@@ -1,13 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
-
-local wgsl_group = vim.api.nvim_create_augroup("WgslFormat", { clear = true })
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.wgsl",
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end,
-  group = wgsl_group,
-})
